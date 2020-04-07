8241fd5e34a270ad38931e45bd8eea030a454c59
ljn <425999212@qq.com>
2020-04-07 20:04:30 +0800

    Initial commit

---
+++ feign/src/main/java/com/janeroad/feign/impl/FeignError.java
@@ -0,0 +1,20 @@
+package com.janeroad.feign.impl;
+
+import com.janeroad.entity.Student;
+import com.janeroad.feign.FeignProviderClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class FeignError implements FeignProviderClient {
+    @Override
+    public Collection<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public String index() {
+        return "服务器维护中......";
+    }
+}
---
+++ hystrix/src/main/java/com/janeroad/feign/FeignProviderClient.java
@@ -0,0 +1,17 @@
+package com.janeroad.feign;
+
+
+import com.janeroad.eneity.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@FeignClient(value = "provider")
+public interface FeignProviderClient {
+    @GetMapping("/student/findAll")
+    public Collection<Student> findAll();
+
+    @GetMapping("/student/index")
+    public String index();
+}
